b1066dc9033391afab570d53b1230734a54e65be
John Burnett <github@johnburnett.com>
2023-04-03 12:49:25 -0700

    Work with readline on Windows

--- pythonrc.py
+++ pythonrc.py
@@ -1,14 +1,9 @@
 # coding=utf-8
-from __future__ import absolute_import as _
-from __future__ import division as _
-from __future__ import print_function as _
-from __future__ import unicode_literals as _
-
 print('Running "%s"' % __file__)
 
 def pythonrc():
     import sys
-    if sys.version_info.major > 2:
+    if sys.version_info.major < 3:
         return
 
     import atexit
@@ -16,45 +11,57 @@ def pythonrc():
     import os
     import platform
 
-    # http://docs.python.org/library/readline.html#module-readline
-    # http://docs.python.org/library/rlcompleter.html
-    # http://valueerror.wordpress.com/2009/11/03/python-shell-history-autocompletion-and-rc-file/
 
-    if platform.system() != 'Windows':
-        def setup_history():
-            """Setup history logging on readline style platforms."""
+    is_windows = platform.system().lower() == 'windows'
+
+
+    def setup_history():
+        """Setup history logging on readline style platforms.
+        """
+        # http://docs.python.org/library/readline.html#module-readline
+        # http://docs.python.org/library/rlcompleter.html
+        # http://valueerror.wordpress.com/2009/11/03/python-shell-history-autocompletion-and-rc-file/
+        try:
             import readline
-            import rlcompleter
+        except ImportError:
+            if is_windows:
+                print('Error importing readline, install pyreadline3:')
+                print(f'{sys.executable} -m pip install pyreadline3')
+                return
+            raise
+        import rlcompleter
 
-            historyFilePath = os.path.expanduser(os.path.join('~', '.python_history'))
-            try:
-                readline.read_history_file(historyFilePath)
-            except:
-                pass
-            readline.parse_and_bind('tab: complete')
-            atexit.register(readline.write_history_file, historyFilePath)
+        historyFilePath = os.path.expanduser(os.path.join('~', '.python_history'))
+        try:
+            readline.read_history_file(historyFilePath)
+        except:
+            pass
+        readline.parse_and_bind('tab: complete')
+        atexit.register(readline.write_history_file, historyFilePath)
 
-            class HistoryConsole(code.InteractiveConsole):
-                def __init__(self, locals=None, filename="<console>", histfile=os.path.expanduser(os.path.join('~', '.python_history'))):
-                    code.InteractiveConsole.__init__(self, locals, filename)
-                    self.init_history(histfile)
+        class HistoryConsole(code.InteractiveConsole):
+            def __init__(self, locals=None, filename="<console>", histfile=os.path.expanduser(os.path.join('~', '.python_history'))):
+                code.InteractiveConsole.__init__(self, locals, filename)
+                self.init_history(histfile)
 
-                def init_history(self, histfile):
-                    readline.parse_and_bind("tab: complete")
-                    if hasattr(readline, "read_history_file"):
-                        try:
-                            readline.read_history_file(histfile)
-                        except IOError:
-                            pass
-                        atexit.register(self.save_history, histfile)
+            def init_history(self, histfile):
+                readline.parse_and_bind("tab: complete")
+                if hasattr(readline, "read_history_file"):
+                    try:
+                        readline.read_history_file(histfile)
+                    except IOError:
+                        pass
+                    atexit.register(self.save_history, histfile)
 
-                def save_history(self, histfile):
-                    readline.write_history_file(histfile)
+            def save_history(self, histfile):
+                readline.write_history_file(histfile)
 
 
-        def wrap_help():
-            """On linux, help() goes into an interactive help browser.
-            Do not want, as it erases help from terminal when exiting."""
+    def wrap_help():
+        """On linux, help() goes into an interactive help browser.
+        Do not want, as it erases help from terminal when exiting.
+        """
+        if not is_windows:
             import pydoc
 
             def print_help(*args, **kwargs):
@@ -66,8 +73,8 @@ def pythonrc():
             help = print_help
 
 
-        setup_history()
-        wrap_help()
+    setup_history()
+    wrap_help()
 
 
 pythonrc()
