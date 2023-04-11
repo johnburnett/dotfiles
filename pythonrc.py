0174fe01c036e08403f2831a23ca44cbe4a904b7
John Burnett <github@johnburnett.com>
2023-04-11 10:16:10 -0700

    tweaks

--- pythonrc.py
+++ pythonrc.py
@@ -1,82 +1,85 @@
 # coding=utf-8
-print('Running "%s"' % __file__)
+import atexit
+import code
+import os
+import platform
+import sys
 
-def pythonrc():
-    import sys
+
+def noop(*args, **kwargs): pass
+info = print
+error = print
+debug = noop
+
+
+def setup_history():
+    """Setup history logging on readline style platforms.
+    """
+    # http://docs.python.org/library/readline.html#module-readline
+    # http://docs.python.org/library/rlcompleter.html
+    # http://valueerror.wordpress.com/2009/11/03/python-shell-history-autocompletion-and-rc-file/
+    try:
+        import readline
+    except ImportError:
+        if platform.system().lower() == 'windows':
+            error('Error importing readline, install pyreadline3:')
+            error(f'{sys.executable} -m pip install pyreadline3')
+            return
+        raise
+    import rlcompleter
+
+    historyFilePath = os.path.expanduser(os.path.join('~', '.python_history'))
+    try:
+        readline.read_history_file(historyFilePath)
+    except:
+        pass
+    readline.parse_and_bind('tab: complete')
+    atexit.register(readline.write_history_file, historyFilePath)
+
+    class HistoryConsole(code.InteractiveConsole):
+        def __init__(self, locals=None, filename="<console>", histfile=os.path.expanduser(os.path.join('~', '.python_history'))):
+            code.InteractiveConsole.__init__(self, locals, filename)
+            self.init_history(histfile)
+
+        def init_history(self, histfile):
+            readline.parse_and_bind("tab: complete")
+            if hasattr(readline, "read_history_file"):
+                try:
+                    readline.read_history_file(histfile)
+                except IOError:
+                    pass
+                atexit.register(self.save_history, histfile)
+
+        def save_history(self, histfile):
+            readline.write_history_file(histfile)
+
+
+def wrap_help():
+    """On linux, help() goes into an interactive help browser.
+    Do not want, as it erases help from terminal when exiting.
+    """
+    if not platform.system().lower() == 'windows':
+        import pydoc
+
+        def print_help(*args, **kwargs):
+            kwargs['title'] = '%s'
+            info(pydoc.render_doc(*args, **kwargs))
+
+        global help, help_original
+        help_original = help
+        help = print_help
+
+
+def main():
     if sys.version_info.major < 3:
         return
-
-    import atexit
-    import code
-    import os
-    import platform
-
-
-    is_windows = platform.system().lower() == 'windows'
-
-
-    def setup_history():
-        """Setup history logging on readline style platforms.
-        """
-        # http://docs.python.org/library/readline.html#module-readline
-        # http://docs.python.org/library/rlcompleter.html
-        # http://valueerror.wordpress.com/2009/11/03/python-shell-history-autocompletion-and-rc-file/
-        try:
-            import readline
-        except ImportError:
-            if is_windows:
-                print('Error importing readline, install pyreadline3:')
-                print(f'{sys.executable} -m pip install pyreadline3')
-                return
-            raise
-        import rlcompleter
-
-        historyFilePath = os.path.expanduser(os.path.join('~', '.python_history'))
-        try:
-            readline.read_history_file(historyFilePath)
-        except:
-            pass
-        readline.parse_and_bind('tab: complete')
-        atexit.register(readline.write_history_file, historyFilePath)
-
-        class HistoryConsole(code.InteractiveConsole):
-            def __init__(self, locals=None, filename="<console>", histfile=os.path.expanduser(os.path.join('~', '.python_history'))):
-                code.InteractiveConsole.__init__(self, locals, filename)
-                self.init_history(histfile)
-
-            def init_history(self, histfile):
-                readline.parse_and_bind("tab: complete")
-                if hasattr(readline, "read_history_file"):
-                    try:
-                        readline.read_history_file(histfile)
-                    except IOError:
-                        pass
-                    atexit.register(self.save_history, histfile)
-
-            def save_history(self, histfile):
-                readline.write_history_file(histfile)
-
-
-    def wrap_help():
-        """On linux, help() goes into an interactive help browser.
-        Do not want, as it erases help from terminal when exiting.
-        """
-        if not is_windows:
-            import pydoc
-
-            def print_help(*args, **kwargs):
-                kwargs['title'] = '%s'
-                print(pydoc.render_doc(*args, **kwargs))
-
-            global help, help_original
-            help_original = help
-            help = print_help
-
 
     setup_history()
     wrap_help()
 
 
-pythonrc()
-del pythonrc
-del __file__
+main()
+for name in list(globals().keys()):
+    if not name.startswith('_'):
+        del globals()[name]
+del name
--- sitecustomize.py
+++ sitecustomize.py
@@ -1,15 +1,17 @@
 # coding=utf-8
-print('Running "%s"' % __file__)
+import os
+import pkgutil
+import site
+import sys
 
-def main():
-    import site
-    import sys
 
-    # Hacky, but "import bpy" doesn't work while sitecustomize is running on
-    # startup, and not sure how else to discover we're in a Blender hosted
-    # Python interpreter.
-    in_blender = 'blender' in sys.executable.lower()
+def noop(*args, **kwargs): pass
+info = print
+error = print
+debug = noop
 
+
+def install_rich():
     try:
         import rich.console
         import rich.traceback
@@ -19,6 +21,10 @@ def main():
         raise
     else:
         console = None
+        # Hacky, but "import bpy" doesn't work while sitecustomize is running on
+        # startup, and not sure how else to discover we're in a Blender hosted
+        # Python interpreter.
+        in_blender = 'blender' in sys.executable.lower()
         if in_blender:
             # Would prefer color tracebacks, but the Python Console in Blender
             # itself is monochrome.  The "stderr=True" comes from how rich
@@ -26,15 +32,28 @@ def main():
             # traceback.install call.
             console = rich.console.Console(stderr=True, color_system=None)
 
+        rich_modules = []
+        import rich
+        for importer, modname, ispkg in pkgutil.iter_modules(rich.__path__):
+            module = __import__(f'{rich.__name__}.{modname}', fromlist='dummy')
+            rich_modules.append(module)
+
         rich.traceback.install(
             console=console,
             show_locals=True,
             locals_hide_dunder=False,
             indent_guides=False,
+            suppress=rich_modules,
         )
-        print('Installed rich traceback handler')
+        debug('Installed rich traceback handler')
+
+
+def main():
+    install_rich()
 
 
 main()
-del main
-del __file__
+for name in list(globals().keys()):
+    if not name.startswith('_'):
+        del globals()[name]
+del name
